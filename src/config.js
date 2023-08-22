18aaeaf0b4b07aebbfec4169a83f3e10c8ee87db
Nagendra9527 <109931776+Nagendra9527@users.noreply.github.com>
2023-08-22 13:56:54 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@
 
 module.exports = {
     api: {
-        invokeUrl: '' // e.g. https://rc2345678345.execute-api.us-west-2.amazonaws.com/prod,
+        invokeUrl: 'https://9qrkl6glf0.execute-api.ap-southeast-1.amazonaws.com/prod' // e.g. https://rc2345678345.execute-api.us-west-2.amazonaws.com/prod,
     }
 }
